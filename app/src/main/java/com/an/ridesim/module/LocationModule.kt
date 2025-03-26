e78b884f30a0d407afb970a488f91997a8bb7a9f
Anitaa Murthy <murthyanitaa@gmail.com>
2025-03-26 11:14:24 +0530

    Fixed Hilt conflict issue by removing @Singleton from both modules

--- app/src/main/java/com/an/ridesim/module/LocationModule.kt
+++ app/src/main/java/com/an/ridesim/module/LocationModule.kt
@@ -7,13 +7,11 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import jakarta.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class LocationModule {
     @Provides
-    @Singleton
     fun provideLocationUtils(
         @ApplicationContext context: Context
     ): LocationUtils = LocationUtils(context)
